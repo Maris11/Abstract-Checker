3416323edfef24f235e2e80299acfc1f9a40a345
Maris11 <mariscinis@gmail.com>
2023-02-19 17:25:37 +0200

    Added ChatGPT

--- chatgpt.py
+++ chatgpt.py
@@ -6,17 +6,20 @@ file = open('abstracts.csv', 'r', encoding='utf-8')
 reader = csv.reader(file)
 titles = []
 rows = list(reader)
-row_id = rows[-1][0] + 1
+row_id = int(rows[-1][0]) + 1
 
-for i, row in enumerate(reader):
+for i, row in enumerate(rows, start=32):
     if i == 100:
         break
     titles.append(row[1])
 
-file = open('abstracts.csv', 'a', encoding='utf-8')
+file.close()
+file = open('abstracts.csv', 'a', encoding='utf-8', newline='')
 writer = csv.writer(file)
 
 for title in titles:
-    response = ai.ask("Uzraksti anotāciju zinātniski pētnieciksajam darbam ar tēmu \"" + title + "\".")
+    response = ai.ask("Uzraksti anotāciju (līdz 850 rakstzīmēm) tēmai \"" + title + "\". Izmanto citādāku anotācijas struktūru.")
+    response = '\n'.join([line for line in response.splitlines() if line.strip()])
+    print(response)
     writer.writerow([row_id, title, response, 1])
     row_id = row_id + 1
--- csv/make_csv.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-rows = [
-    ['id', 'title', 'abstract_latvian', 'is_generated']
-]
-
-with open('../abstracts.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(rows)
